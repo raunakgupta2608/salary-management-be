cb3f33318be4d14a0e47ed9f51b6aa5e9cfa691a
Raunak Gupta <raunakgupta2608@gmail.com>
2026-05-31 19:42:27 +0530

    cors-fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://salary-management-fe-omega.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
